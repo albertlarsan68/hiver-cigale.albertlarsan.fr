49676798d3abf98ba884b401b338b3ac2436ef3a
AxelBattigelli <axel.battigelli@epitech.eu>
2024-06-03 16:13:47 +0200

    move folder

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import qrcode from './components/qrCode';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+      </header>
+      <qrcode></qrcode>
+    </div>
+  );
+}
+
+export default App;
