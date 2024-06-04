6572d455bba95b38d8fa9a8ffdeff937d44b1b38
AxelBattigelli <axel.battigelli@epitech.eu>
2024-06-04 13:45:17 +0200

    test version

--- src/pages/Lequipe.js
+++ src/pages/Lequipe.js
@@ -1,24 +1,24 @@
-// import React from 'react';
+import React from 'react';
 // import QrCode from '../components/QrCode';
 import '../styles/Lequipe.css';
 import dev1 from './../images/axel.battigelli@epitech.eu.jpg';
 import dev2 from './../images/tissia.eckling@epitech.eu.jpg';
 import dev3 from './../images/anicet.ottmann-florentin@epitech.eu.jpg';
 
-import React, { useState, useEffect } from 'react';
-import Loading from './../components/Loading';
+// import React, { useState, useEffect } from 'react';
+// import Loading from './../components/Loading';
 
-function runFunction(setIsLoading) {
-    console.log("pass");
-    if (document.readyState == "complete") {
-        setIsLoading = false;
-    };
-    return setIsLoading;
-}
+// function runFunction(setIsLoading) {
+//     console.log("pass");
+//     if (document.readyState == "complete") {
+//         setIsLoading = false;
+//     };
+//     return setIsLoading;
+// }
 
 function Lequipe() {
-    const setIsLoading = true;
-    var t = setInterval(runFunction(setIsLoading), 1000);
+    // const setIsLoading = true;
+    // var t = setInterval(runFunction(setIsLoading), 1000);
    
     // if (setIsLoading) {
     //     return <Loading />;
