ca8b0d5a5bd8e008bb43b78cf872121c6c868db9
AxelBattigelli <axel.battigelli@epitech.eu>
2024-06-04 15:43:11 +0200

    test version

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Footer from './components/Footer';
 import Home from './pages/Home';
 import Lequipe from './pages/Lequipe';
 import Jeu from './pages/Jeu';
+import NotFound from './pages/NotFound.js';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route path='/' element={<Home />} />
           <Route path='/lequipe' element={<Lequipe />} />
           <Route path='/jeu' element={<Jeu />} />
+          <Route path='/*' element={<NotFound />} />
         </Routes>
         <Footer />
       </HashRouter>
---
+++ src/pages/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function NotFound() {
+    return (
+        <div className="NotFound">
+            404 Not Found
+        </div>
+    );
+}
+
+export default NotFound;
